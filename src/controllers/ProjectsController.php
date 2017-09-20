e43dc60b3c0a4d3f312f65608d9a999b74199368
Donii Sergii <doniysa@gmail.com>
2017-09-20 23:48:17 +0300

    contact model

--- src/controllers/ProjectsController.php
+++ src/controllers/ProjectsController.php
@@ -8,10 +8,18 @@
 
 namespace sonrac\i18n\controllers;
 
+use sonrac\i18n\models\search\ProjectsRepository;
+use Yii;
 
 class ProjectsController extends \yii\web\Controller
 {
+    /**
+     * @throws \yii\base\InvalidConfigException
+     * @throws \yii\di\NotInstantiableException
+     *
+     * @author Donii Sergii <doniysa@gmail.com>
+     */
     public function actionIndex() {
-
+        $searchModel = Yii::$container->get(ProjectsRepository::class);
     }
 }
